38ae8c1dea09183644abd13801765c4eef720ea8
tzshin <kabonplay@gmail.com>
2023-09-29 17:51:56 +0800

    Remove secrets.h

--- src/secrets.h
+++
@@ -1,8 +0,0 @@
-// Arduino IoT cloud
-#define SSID       "Hung's Airport TC "
-#define PASS       "19552011apple"
-#define DEVICE_ID  "ca239f00-ec39-47dd-a922-f1c3e634ee74"
-#define DEVICE_KEY "WZUVPMTSATN5329KUR9U"
-
-// PC side
-#define PC_PWD     "2225"
